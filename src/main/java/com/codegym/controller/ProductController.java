28e2aa11f36321f53323d7472b8d2d0bbdf629af
Thang0226 <thang.nd0226@gmail.com>
2024-12-18 09:11:57 +0700

    can view product list & add new product

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,16 +1,46 @@
 package com.codegym.controller;
 
+import com.codegym.model.Product;
 import com.codegym.service.IProductService;
 import com.codegym.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
 	@Autowired
 	private IProductService productService;
 
+	@GetMapping
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView("list");
+		List<Product> products = productService.findAll();
+		mv.addObject("products", products);
+		return mv;
+	}
 
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("product", new Product());
+		return "create";
+	}
+	@PostMapping("/save")
+	public String save(Product product, RedirectAttributes redirectAttributes) {
+		do {
+			product.setId((int) (Math.random() * 10000));
+		} while (productService.findById(product.getId()) != null);
+		productService.add(product);
+		redirectAttributes.addFlashAttribute("success", "Product successfully added");
+		return "redirect:/products";
+	}
 }
